a26c9ab6115296bdd60eee2620999eb385e6dd53
Junior <jjalves96@gmail.com>
2020-08-27 05:55:00 -0300

    start project

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Todo from './Todo';
+
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Todo />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
